3acf5eac47163a207f1ac9362e67ed8f7218ae83
Thales Paixão <61071029+PaixaoThales@users.noreply.github.com>
2025-07-10 15:23:24 -0300

    Fix memory leakage

--- src/main/java/expressionparser/Launcher.java
+++ src/main/java/expressionparser/Launcher.java
@@ -7,19 +7,20 @@ import java.util.Scanner;
 public class Launcher {
 
     public static void main(String[] args) {
-        Interpreter interpreter = new Interpreter();
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.print(">> ");
-            String input = scanner.nextLine();
-            if (input.isBlank()) {
-                continue;
+        try (Scanner scanner = new Scanner(System.in)) {
+            Interpreter interpreter = new Interpreter();
+            while (true) {
+                System.out.print(">> ");
+                String input = scanner.nextLine();
+                if (input.isBlank()) {
+                    continue;
+                }
+                if (input.trim().equals("exit")) {
+                    System.out.println("Exiting...");
+                    break;
+                }
+                System.out.println(interpreter.execute(input).value());
             }
-            if (input.trim().equals("exit")) {
-                System.out.println("Exiting...");
-                break;
-            }
-            System.out.println(interpreter.execute(input).value());
         }
     }
 }
